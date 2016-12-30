7016b53ac54cc9123c11917126b811ac003851ad
albertovilva <albert.vilva@gmail.com>
2016-12-30 23:30:41 +0100

    feat(Initial Commit):

---
+++ src/app.js
@@ -0,0 +1,6 @@
+import p5 from 'p5';
+import sketch from './js/sketch';
+import './styles/styles.scss';
+
+// Initialize sketch
+new p5(sketch);
---
+++ webpack.config.js
@@ -0,0 +1,44 @@
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+var CopyWebpackPlugin = require('copy-webpack-plugin');
+var ExtractTextPlugin = require("extract-text-webpack-plugin");
+
+module.exports = {
+    entry: './src/app.js',
+    output: {
+        path: './dist',
+        filename: 'bundle.js'
+    },
+    devServer: {
+        inline: true,
+        port: 8888,
+    },
+    module: {
+        loaders: [{
+            test: /\.js$/,
+            exclude: /node_modules/,
+            loader: 'babel-loader',
+            query: {
+                presets: ['es2015']
+            }
+        },{
+          test: /\.(scss|sass)$/,
+          loader: ExtractTextPlugin.extract(
+            'style', // The backup style loader
+            'css?sourceMap!sass?sourceMap'
+            ) }]
+    },
+    resolve: {
+        extensions: [ '', '.js', '.jpg', '.png', '.gif', '.ttf']
+    },
+    plugins: [
+        new CopyWebpackPlugin([
+            {from: './src/assets', to: './assets' }
+        ]),
+        new HtmlWebpackPlugin({
+            title: 'p5-es6-webpack-seed',
+            template: './src/index.html',
+            inject: 'head'
+        }),
+        new ExtractTextPlugin("assets/styles.css")
+    ]
+}
